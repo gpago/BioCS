f5cbb12991b4eefa6ba921f5d9c32ec0f0d951a7
Nikos Kapantais <nikoskapantais7@gmail.com>
2017-06-23 15:15:49 +0300

    Initial Commit

---
+++ Exr14.py
@@ -0,0 +1,31 @@
+class Exr14:
+
+    brain = ""
+    muscle = ""
+
+    def readFiles(self):
+        global brain
+        global muscle
+        brain = open("BioData\BrainSqn", 'r').read()
+        muscle = open("BioData\MuscleSqn", 'r').read()
+
+    def main(self):
+        print("Starting..")
+
+        self.readFiles(self)
+        print(brain)
+        print("-----------------------------")
+        print(muscle)
+
+
+        
+
+
+
+
+
+
+
+
+
+Exr14.main(Exr14)
