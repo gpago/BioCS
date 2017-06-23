1cb14bb9a07f5962b850e4f14f65317a1e0e4ee5
Nikos Kapantais <nikoskapantais7@gmail.com>
2017-06-23 15:25:14 +0300

    Exercise 15 code

---
+++ Exr15.py
@@ -0,0 +1,28 @@
+class Exr15:
+
+    sqn = ""
+
+    def readFiles(self):
+        global sqn
+        sqn = open("BioData\Alactalbumin", 'r').read()
+
+    def main(self):
+        print("Starting..")
+        self.readFiles(self)
+        print(sqn)
+
+    def player1(self):
+        print("player1 code")
+
+    def player2(self):
+        print("player2 code")
+
+
+
+
+
+
+
+
+
+Exr15.main(Exr15)
