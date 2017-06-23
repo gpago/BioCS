8e7ed0ba55d1db873307b8879bc0cd57a98ddd13
Nikos Kapantais <nikoskapantais7@gmail.com>
2017-06-23 15:45:33 +0300

    Data for exercise 22

--- Exr22.py
+++ Exr22.py
@@ -2,16 +2,21 @@
 
 class Exr22:
     sqn = ""
+    lyz = ""
 
     def readFiles(self):
         global sqn
+        global lyz
         sqn = open("BioData\Alactalbumin", 'r').read()
+        lyz = open("BioData\LYZC", 'r').read()
 
 
     def main(self):
         print("Starting..")
         self.readFiles(self)
         print(sqn)
+        print("-----------------")
+        print(lyz)
 
 
 
